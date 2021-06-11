d467caa937781f40185043c1d5ced444642bfdf5
haotianhuang@gmail.com <HHT964250613nce8>
2021-06-11 17:45:25 +1000

    Initial Commit

---
+++ War Challenge/War_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_ChallengeApp.swift
+//  War Challenge
+//
+//  Created by Haotian Huang on 17/5/21.
+//
+
+import SwiftUI
+
+@main
+struct War_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
